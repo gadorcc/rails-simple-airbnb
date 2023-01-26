ac519e34ce781b93a21101e9e28e4707e32c4129
Gador Cruz <gadorcruz@hotmail.com>
2023-01-26 20:24:32 +0000

    I can add flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,41 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path(@flat)
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def set_flat
+    @flat = flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+i = 0
+10.times do
+  i += 1
+  puts "Creating flat number #{i}"
+  Flat.create(
+    name: "Light & Spacious Garden Flat London",
+    address: Faker::Address.street_name,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(50..300),
+    number_of_guests: rand(1..9)
+  )
+end
